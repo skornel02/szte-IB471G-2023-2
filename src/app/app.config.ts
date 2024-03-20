213f791446bfe41234816be660ba30c509437470
Kornél <stefankornel02@gmail.com>
2024-03-20 19:31:54 +0100

    Added Firebase Configured deploy Added prettier

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+    providers: [
+        provideRouter(routes),
+        provideClientHydration(),
+        importProvidersFrom(
+            provideFirebaseApp(() =>
+                initializeApp({
+                    projectId: 'router-ui-szte',
+                    appId: '1:943762268884:web:840df8a7be533095651141',
+                    storageBucket: 'router-ui-szte.appspot.com',
+                    apiKey: 'AIzaSyAsGwKQ2cx8wh1heALDReFdpBQjpVYlrVo',
+                    authDomain: 'router-ui-szte.firebaseapp.com',
+                    messagingSenderId: '943762268884',
+                })
+            )
+        ),
+        importProvidersFrom(provideAuth(() => getAuth())),
+        importProvidersFrom(provideFirestore(() => getFirestore())),
+    ],
 };
